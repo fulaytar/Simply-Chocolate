3d9c9ebb4a5735e6f1599153eba21e963249d837
fulaytar <fulaytar2405@gmail.com>
2024-04-13 22:32:00 +0300

    дороби слайдери

--- js/person.js
+++ js/person.js
@@ -1,54 +1,52 @@
-
-
 //тут створили слайдер
-const splide = new Splide('#products', {
-  type: 'loop',
-  perPage: 2,
-  perMove: 1,
+const splide = new Splide("#products", {
+  type: "loop",
   updateOnMove: true,
   speed: 600,
-  easing: 'ease-in',
+  easing: "ease-in",
   pagination: true,
   //властивості до 767
   breakpoints: {
-	767: {
-      perPage: 1,
-      padding: '10%',
-	},
-    
-  }
-
+    767: {
+      fixedWidth: "335px",
+      fixedHeight: "450px",
+    },
+    1200: {
+      fixedWidth: "270x",
+      fixedHeight: "580px",
+    },
+    1920: {
+      fixedWidth: "270x",
+      fixedHeight: "590px",
+    },
+  },
 });
 //тут шукаємо всі елементи
 
-      const thumbnails = document.getElementsByClassName("thumbnail");
-      let current;
-      //ініціалізація
-      for (var i = 0; i < thumbnails.length; i++) {
-        initThumbnail(thumbnails[i], i);
-      }
-      //по індексу шукаємо
-      function initThumbnail(thumbnail, index) {
-        thumbnail.addEventListener("click", function () {
-          splide.go(index);
-        });
-      }
+const thumbnails = document.getElementsByClassName("thumbnail");
+let current;
+//ініціалізація
+for (var i = 0; i < thumbnails.length; i++) {
+  initThumbnail(thumbnails[i], i);
+}
+//по індексу шукаємо
+function initThumbnail(thumbnail, index) {
+  thumbnail.addEventListener("click", function () {
+    splide.go(index);
+  });
+}
 
-      splide.on("mounted move", function () {
-        var thumbnail = thumbnails[splide.index];
+splide.on("mounted move", function () {
+  var thumbnail = thumbnails[splide.index];
 
-        if (thumbnail) {
-          if (current) {
-            current.classList.remove("is-active");
-          }
+  if (thumbnail) {
+    if (current) {
+      current.classList.remove("is-active");
+    }
 
-          thumbnail.classList.add("is-active");
-          current = thumbnail;
-        }
-      });
+    thumbnail.classList.add("is-active");
+    current = thumbnail;
+  }
+});
 
 splide.mount();
-      
-
-
-
