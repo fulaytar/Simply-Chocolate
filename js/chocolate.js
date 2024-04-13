4749da4a3e66aeb50d1f1cf1b327e4a3b56e14a1
fulaytar <fulaytar2405@gmail.com>
2024-04-14 01:51:36 +0300

    дороби слайдер  і модалки

--- js/chocolate.js
+++ js/chocolate.js
@@ -1,50 +1,45 @@
-
-
 //тут створили слайдер
-const persons = new Splide('#review', {
-  type: 'loop',
+const persons = new Splide("#review", {
+  type: "loop",
   perPage: 2,
   perMove: 1,
   updateOnMove: true,
   speed: 600,
-  easing: 'ease-in',
+  easing: "ease-in",
   pagination: true,
   //властивості до 767
   breakpoints: {
-	767: {
+    767: {
       perPage: 1,
-	},
-    
-  }
-
+    },
+  },
 });
 //тут шукаємо всі елементи
-      const personBox=document.getElementById('review')
-      const thumbnails = personBox.getElementsByClassName("thumbnail");
-      let current;
-      //ініціалізація
-      for (var i = 0; i < thumbnails.length; i++) {
-        initThumbnail(thumbnails[i], i);
-      }
-      //по індексу шукаємо
-      function initThumbnail(thumbnail, index) {
-        thumbnail.addEventListener("click", function () {
-          persons.go(index);
-        });
-      }
+const personBox = document.getElementById("review");
+const thumbnails = personBox.getElementsByClassName("thumbnail");
+let current;
+//ініціалізація
+for (var i = 0; i < thumbnails.length; i++) {
+  initThumbnail(thumbnails[i], i);
+}
+//по індексу шукаємо
+function initThumbnail(thumbnail, index) {
+  thumbnail.addEventListener("click", function () {
+    persons.go(index);
+  });
+}
 
-      persons.on("mounted move", function () {
-        var thumbnail = thumbnails[persons.index];
+persons.on("mounted move", function () {
+  var thumbnail = thumbnails[persons.index];
 
-        if (thumbnail) {
-          if (current) {
-            current.classList.remove("is-active");
-          }
+  if (thumbnail) {
+    if (current) {
+      current.classList.remove("is-active");
+    }
 
-          thumbnail.classList.add("is-active");
-          current = thumbnail;
-        }
-      });
+    thumbnail.classList.add("is-active");
+    current = thumbnail;
+  }
+});
 
 persons.mount();
-      
